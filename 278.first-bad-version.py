ef3f07e030dad33fed466fcd3d7e74f7f1b78aff
FrederikNiesner <frederik.niesner@gmail.com>
2021-10-19 11:57:11 +0200

    704 complete

---
+++ 278.first-bad-version.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=278 lang=python
+#
+# [278] First Bad Version
+#
+
+# @lc code=start
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+# @lc code=end
+
