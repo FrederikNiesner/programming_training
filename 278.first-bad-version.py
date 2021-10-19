985129f5e8d9044b41670aaf4f8055fab8205167
FrederikNiesner <frederik.niesner@gmail.com>
2021-10-19 13:34:04 +0200

    278 complete

--- 278.first-bad-version.py
+++ 278.first-bad-version.py
@@ -17,5 +17,16 @@ class Solution(object):
         :rtype: int
         """
         
+        # n: int list, number of versions
+        left, right = 0, n
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid) == True:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
 # @lc code=end
 
--- 704.binary-search.py
+++ 704.binary-search.py
@@ -11,6 +11,11 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: int
+
+        Binary search is a textbook algorithm based on the idea to compare the target value to the middle element of the array.
+        If the target value is equal to the middle element - we're done.
+        If the target value is smaller - continue to search on the left.
+        If the target value is larger - continue to search on the right.
         """
         left, right = 0, len(nums) - 1
         while left <= right:
