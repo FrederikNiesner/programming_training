58ad49ddce59919e27ac85b2f426ef4e5724da25
FrederikNiesner <frederik.niesner@gmail.com>
2021-10-24 17:34:22 +0200

    167 two sum II complete

---
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode id=167 lang=python
+#
+# [167] Two Sum II - Input array is sorted
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        """        
+        # two-pointer
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l + 1, r + 1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
+
+        
+        """
+        
+        # dictionary
+        dic = {}
+        for i, num in enumerate(numbers):
+            if target - num in dic:
+                return [dic[target - num] + 1, i + 1]
+            dic[num] = i
+        
+        """
+        
+        # binary search        
+    
+        for i in xrange(len(numbers)):
+            l, r = i+1, len(numbers)-1
+            tmp = target - numbers[i]
+            while l <= r:
+                mid = l + (r-l)//2
+                if numbers[mid] == tmp:
+                    return [i+1, mid+1]
+                elif numbers[mid] < tmp:
+                    l = mid+1
+                else:
+                    r = mid-1
+        """
+# @lc code=end
+
