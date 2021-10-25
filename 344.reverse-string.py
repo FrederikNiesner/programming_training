4d478a35f4ebd8d99cfb67b2aba208d0e6099c05
FrederikNiesner <frederik.niesner@gmail.com>
2021-10-25 20:24:04 +0200

    344 alternatives

--- 344.reverse-string.py
+++ 344.reverse-string.py
@@ -11,12 +11,24 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        left, right = 0, len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+        # left, right = 0, len(s) - 1
+        # while left < right:
+        #     s[left], s[right] = s[right], s[left]
+        #     left += 1
+        #     right -= 1
         
-        return s
+        # return s
+
+        # Recursion Alternative
+        # def helper(left, right):
+        #     if left < right:
+        #         s[left], s[right] = s[right], s[left]
+        #         helper(left + 1, right - 1)
+
+        # helper(0, len(s) - 1)
+
+        # faster
+        
+        # return ' '.join(x[::-1] for x in s.split())
 # @lc code=end
 
