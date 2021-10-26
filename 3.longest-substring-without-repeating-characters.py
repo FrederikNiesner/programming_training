02b4d8bc8a38ea9487a40532d8f1739cf3f77e9e
FrederikNiesner <frederik.niesner@gmail.com>
2021-10-26 18:36:15 +0200

    3 in progress + added test file

---
+++ 3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=3 lang=python
+#
+# [3] Longest Substring Without Repeating Characters
+#
+
+# @lc code=start
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+
+        def lengthOfLongestSubstring(s):
+
+            
+# @lc code=end
+
